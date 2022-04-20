75ff87fbb3b83671e1ab9210c2ed7f59e9af37cf
Pedro Leite <pgleite1990@gmail.com>
2022-04-20 19:20:42 +0100

    conexão à base dados da página details e definição dos params

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,3 +1,25 @@
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
 export default function Detail() {
-  return <section>teste</section>;
+  const params = useParams();
+  const [stock, setStock] = useState();
+
+  useEffect(() => {
+    fetch("https://justivo.com/stockws.php?get&code=BST")
+      .then((response) => response.json())
+      .then((dataReceived) => setStock(dataReceived));
+  }, []);
+
+  return (
+    <>
+      {stock && (
+        <section>
+          <h2>{stock.name}</h2>
+          <h3>{stock.code}</h3>
+          <div>Preço: {stock.price}€</div>
+        </section>
+      )}
+    </>
+  );
 }

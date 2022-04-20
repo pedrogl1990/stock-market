63768e754c7c71eea673b5f6f831a7db32c5d439
Pedro Leite <pgleite1990@gmail.com>
2022-04-20 19:56:03 +0100

    criação do input no create

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,3 +1,57 @@
 export default function Create() {
-  return <div>Create</div>;
+  function handleChange(e) {
+    console.log("handleChange: ", e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Nome
+            <input
+              onChange={handleChange}
+              type="text"
+              name="name"
+              required
+              minLength="4"
+              maxLength="60"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Código
+            <input
+              onChange={handleChange}
+              type="text"
+              name="code"
+              required
+              minLength="3"
+              maxLength="3"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Preço
+            <input
+              onChange={handleChange}
+              type="number"
+              name="price"
+              required
+              minLength="1"
+              maxLength="9999"
+            />
+          </label>
+        </div>
+      </form>
+      <button type="submit">Guardar</button>
+    </main>
+  );
 }
